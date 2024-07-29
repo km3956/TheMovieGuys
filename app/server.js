9f34084193de1256fd8b4f15593202e279d478d4
ami52 <ami52@drexel.edu>
2024-07-29 13:52:55 -0400

    Adding skeleton files

---
+++ app/server.js
@@ -0,0 +1,9 @@
+let axios = require("axios");
+let express = require("express");
+let app = express();
+let apiFile = require("../env.json");
+let apiKey = apiFile["api_key"]; 
+let baseUrl = apiFile["api_url"];
+let port = 3000;
+let hostname = "localhost";
+app.use(express.static("public"));
