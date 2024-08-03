0a4edba43b6e294ec71fa09ae7e8cae4773875df
Asiful Islam <167476136+BlazerShinobi@users.noreply.github.com>
2024-08-02 23:38:13 -0400

    Adding home page and centering nav bar

--- app/server.js
+++ app/server.js
@@ -5,8 +5,13 @@ let apiKey = apiFile["api_key"];
 let baseUrl = apiFile["api_url"];
 let port = 3000;
 let hostname = "localhost";
+let path = require("path");
 app.use(express.static("public"));
 
 app.listen(port, hostname, () => {
     console.log(`http://${hostname}:${port}`);
-  });
+});
+
+app.get('/env.json', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'env.json'));
+});
